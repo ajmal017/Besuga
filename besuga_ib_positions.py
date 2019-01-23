50c0a14e47efa0107e120ddec4ff646f1eb52f77
besugatrading <44233254+besugatrading@users.noreply.github.com>
2019-01-23 14:22:11 +0100

    Add files via upload

--- besuga_ib_positions.py
+++ besuga_ib_positions.py
@@ -29,8 +29,8 @@ def get_contracts(ib):
             lst2.append(contr.strike)                           #lst2[7]
             lst2.append(contr.right)                            #lst2[8]
             lst2.append(contr.multiplier)                       #lst2[9]
-        elif (contr.secType == 'STK'):
-            lst2.extend([None, None, None, 1])                  # posem el multiplier a 1 per Stocks
+        else:
+            lst2.extend([None, None, None, 1])                  # posem el multiplier a 1 per la resta d'instruments
         lst.append(lst2)
     return(lst)
 
@@ -78,9 +78,7 @@ def get_executions(ib):
                 opt = ib.reqMktData(execs[i].contract, '', False, False)
                 ib.sleep(5)
                 l += 1
-                print ('OPT blucle', opt)
             opt = ib.reqMktData(execs[i].contract, '100,101,105,106,107', False, False)
-            print ('OPT fora blucle', opt)
             if (opt.lastGreeks is not None):
                 lst2.append(opt.lastGreeks.optPrice)           #lst2[8]
                 lst2.append(opt.lastGreeks.impliedVol)         #lst2[9]
@@ -120,13 +118,13 @@ def get_openpositions(ib):
     return (lst)
 
 
-def dbconnect():
+def dbconnect(dbname="besuga"):
     try:
         cnx = mysql.connector.connect(
             host='localhost',
             user='xarnaus',
             passwd='Besuga8888',
-            database='besuga'
+            database=dbname
         )
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
@@ -216,42 +214,42 @@ def dbanalyse_positions(db, accId):
         lst = execute_query(db, sql)            # llista els diferents contractes a 'combinedtrades'
         final_list=[]
         for i in range(len(lst)):
-            sql = "SELECT ctId, ctExecId, ctConId, ctType, ctMultiplier, ctShares, ctPrice, ctDate, ctCommission, ctLiquidation, ctoptPrice, ctoptIV, ctoptDelta, ctoptGamma, " \
+            sql = "SELECT ctId, ctAccId, ctExecId, ctConId, ctType, ctMultiplier, ctShares, ctPrice, ctDate, ctCommission, ctLiquidation, ctoptPrice, ctoptIV, ctoptDelta, ctoptGamma, " \
                     + "ctoptTheta, ctoptVega, ctoptPVDividend, ctoptPriceOfUnderlying, ctActive FROM combinedtrades " \
                     + "WHERE ctAccId = '" + str(accId) + "' AND ctConId = " + str(lst[i][0]) + " ORDER BY ctActive, ctTime"
             execs = execute_query(db, sql)
             # mirem si l'últim registre està actiu (com a molt pot ser l'últim), si està actiu no cal fer-li res
             stop = len(execs)
-            if execs[len(execs)-1][18] == 1:  stop = len(execs)-1
+            if execs[len(execs)-1][19] == 1:  stop = len(execs)-1
             for h in range(0, len(execs)):  execs[h] = list(execs[h])  # convertim la tupla en una list
             j, new_k, new_j = 0, stop, stop
-            for h in (y for y in range(j + 1, stop) if sign(execs[y][5]) != sign(execs[j][5])):
+            for h in (y for y in range(j + 1, stop) if sign(execs[y][6]) != sign(execs[j][6])):
                 new_k = h
                 break
             k = min(new_k, stop)
             while j < stop:
-                if abs(execs[j][5]) < abs(execs[k][5]):         # Comparació de les +/- shares
-                    execs[j].append(execs[k])                   # append a la llista de j tota la llista de k com exec[j][19]
-                    execs[k][5] = execs[j][5] + execs[k][5]     # recalculem el número de shares de k per la següent iteració
-                elif abs(execs[j][5]) == abs(execs[k][5]):
-                    execs[k][18] = 'D'                          # D for delete
-                    execs[j].append(execs[k])                   # append a la llista de j tota la llista de k com exec[j][19]
+                if abs(execs[j][6]) < abs(execs[k][6]):         # Comparació de les +/- shares
+                    execs[j].append(execs[k])                   # append a la llista de j tota la llista de k com exec[j][20]
+                    execs[k][6] = execs[j][6] + execs[k][6]     # recalculem el número de shares de k per la següent iteració
+                elif abs(execs[j][6]) == abs(execs[k][6]):
+                    execs[k][19] = 'D'                          # D for delete
+                    execs[j].append(execs[k])                   # append a la llista de j tota la llista de k com exec[j][20]
                     # en aquest cas (k<j), tanquem execs[j]iinserim un nou element a la lliata amb la resta de shares de exec[j]
                     # ajustem la variable stop adequadament
                 else:
-                    execs[k][18] = 'D'                          # D for delete
+                    execs[k][19] = 'D'                          # D for delete
                     aux = execs[j].copy()                       # aux és una llista auxiliar
                     aux[0] = execs[k][0]                        # utilitzem l'id de K (doncs sabem que execs[k] tanc auna posició, l'id no s'usarà
-                    aux[5] = execs[j][5] + execs[k][5]          # el número de shares que quedaran al nou element
-                    execs[j][5] = - execs[k][5]                 # ajustem el número de shares a execs[j] - posició que tanca
-                    execs[j].append(execs[k])                   # append a la llista de j tota la llista de k com exec[j][19]
+                    aux[6] = execs[j][6] + execs[k][6]          # el número de shares que quedaran al nou element
+                    execs[j][6] = - execs[k][6]                 # ajustem el número de shares a execs[j] - posició que tanca
+                    execs[j].append(execs[k])                   # append a la llista de j tota la llista de k com exec[j][20]
                     execs.insert(j+1,aux)                       # insertem el nou element a la posició j+1
                     stop += 1                                   # stop augmenta en un doncs afegim un element a la execs
-                execs[j][18] = 'C'                                  # posarem el registre a tActive = (C)losed - SEMPRE
+                execs[j][19] = 'C'                                  # posarem el registre a tActive = (C)losed - SEMPRE
                 new_j = stop
-                for h in (x for x in range(j+1, stop) if execs[x][18] != 'D'):
+                for h in (x for x in range(j+1, stop) if execs[x][19] != 'D'):
                     new_j = h
-                    for l in (y for y in range(new_j + 1, stop) if sign(execs[y][5]) != sign(execs[new_j][5])):
+                    for l in (y for y in range(new_j + 1, stop) if sign(execs[y][6]) != sign(execs[new_j][6])):
                         k = l
                         break
                     break
@@ -322,20 +320,17 @@ def dbupdate_executions(db, execs):
 
 # ATENCIÓ: S'HA DE MODIFICAR PERQUÈ EL DELETE TINGUI EN COMPTE l'ACCID
 def dbfill_positions(db, execs):
-    # execs[i] conté [ctId, ctExecId, ctConId, ctType, ctMultiplier, ctShares, ctPrice, ctDate, ctCommission, ctLiquidation, ctoptPrice, ctoptIV, ctoptDelta, ctoptGamma,
+    # execs[i] conté [ctId, ctAccId, ctExecId, ctConId, ctType, ctMultiplier, ctShares, ctPrice, ctDate, ctCommission, ctLiquidation, ctoptPrice, ctoptIV, ctoptDelta, ctoptGamma,
     # ctoptTheta, ctoptVega, ctoptPVDividend, ctoptPriceOfUnderlying, ctActive
     # Si ctActive='C', llavors execs[i][19]=[execs[j]], on execs[j] és l'execució que tanca execs[i]
     try:
-        # borrem la taula de positions)position_optiondetails i després farem inserts del què tenim a execs
-        sql = "DELETE FROM positions_optiondetails where "
-        count = execute_query(db, sql, commit = False)
-        print('Deleted ' + str(count) + ' rows in table positions_optiondetails')
-        sql = "DELETE FROM positions WHERE pAccId = "
+        # borrem la taula de positions (per aquest Account) i després farem inserts del què tenim a execs. La taula positions_optiondetails es borra també (DELETE CASCADE)
+        sql = "DELETE FROM positions WHERE pAccId = '" + str(execs[0][1]) + "'"
         count = execute_query(db, sql, commit = False)
         print('Deleted ' + str(count) + ' rows in table positions')
         dbcommit(db)
         for i in range(len(execs)):
-            if execs[i][18] != 'D':
+            if execs[i][19] != 'D':
                 print ('Inserting position ' + str(execs[i][0]))
                 sql = "INSERT INTO positions (pId, pExecid, pAccId, pConId, pDate, pType, pMultiplier, pShares, pInitialPrice, pInitialValue, pCommission, pLiquidation, pActive) " \
                     + "SELECT ctId, ctExecId, ctAccId, ctConId, ctDate, ctType, ctMultiplier, ctShares, ctPrice, ctPrice*abs(ctShares)*ctMultiplier, ctCommission, ctLiquidation, ctActive " \
@@ -347,18 +342,18 @@ def dbfill_positions(db, execs):
                         + "SELECT ctId, ctoptPrice, ctoptIV, ctoptDelta, ctoptGamma, ctoptVega, ctoptTheta, ctoptPVDividend, ctoptPriceOfUnderlying " \
                         + "FROM combinedtrades WHERE ctID = " + str(execs[i][0])
                     execute_query(db, sql)
-                if execs[i][18] == 'C':
-                    clist = execs[i][19]
+                if execs[i][19] == 'C':
+                    clist = execs[i][20]
                     sql = "UPDATE positions set pActive = %s, pClosingPrice = %s, pClosingValue = %s, pClosingDate = %s, pClosingId = %s, pPNL = %s, pCommission = %s, pLiquidation = %s " \
                         + "WHERE pId = " + str(execs[i][0])
-                    pnl = -(execs[i][5]*execs[i][6] + clist[5]*clist[6])*execs[i][4]
-                    val = (0, clist[6], clist[4] * abs(clist[5]) * clist[6], clist[7], clist[0], pnl, execs[i][8] + clist[8], clist[9])
+                    pnl = -(execs[i][6]*execs[i][7] + clist[6]*clist[7])*execs[i][5]
+                    val = (0, clist[7], clist[5] * abs(clist[6]) * clist[7], clist[8], clist[0], pnl, execs[i][9] + clist[9], clist[10])
                     execute_query(db, sql, values = val)
-                    if execs[i][3] == 'OPT':
+                    if execs[i][4] == 'OPT':
                         sql = "UPDATE positions_optiondetails set podFinalModelPrice = %s, podFinalIV = %s, podFinalDelta = %s, podFinalGamma = %s, podFinalTheta = %s,  " \
                             + "podFinalVega = %s, podFinalPVDividend = %s, podFinalPriceOfUnderlying = %s " \
                             + "WHERE podId = " + str(execs[i][0])
-                        val = (clist[10], clist[11], clist[12], clist[13], clist[14], clist[15], clist[16], clist[17])
+                        val = (clist[11], clist[12], clist[13], clist[14], clist[15], clist[16], clist[17], clist[18])
                         execute_query(db, sql, values = val)
     except Exception as err:
         print(err)
@@ -368,18 +363,23 @@ def dbfill_positions(db, execs):
 if __name__ == '__main__':
 
     myib = IB()
-    mydb = dbconnect()
+    mydb = dbconnect("besuga")
 
-    #rslt = execute_query(mydb,"SELECT connHost, connPort, connAccId FROM connections WHERE connName = 'xavpaper7497'")
-    rslt = execute_query(mydb, "SELECT connHost, connPort, connAccId FROM connections WHERE connName = 'besugapaper7498'")
+    #  creem instancia de connexió db al mateix temps que triem compte a IB amb el que operem
+
+    acc = input("triar entre 'besugapaper' o 'xavpaper' ")
+    if acc == "besugapaper":
+        rslt = execute_query(mydb,"SELECT connHost, connPort, connAccId FROM connections WHERE connName = 'besugapaper7498'")
+    elif acc == "xavpaper":
+        rslt = execute_query(mydb, "SELECT connHost, connPort, connAccId FROM connections WHERE connName = 'xavpaper7497'")
+    else:
+        print("Unknown account!!")
     myib.connect(rslt[0][0], rslt[0][1], 1)
     myaccId = rslt[0][2]
 
 
     dbfill_contracts(mydb, get_contracts(myib))                     # inserta tots els diferents contractes, si no hi són a la DB
-    aux = get_executions(myib)
-    print(aux)
-    dbfill_executions(mydb, aux)                   # inserta les noves (de 1 a 7 dies) executions
+    dbfill_executions(mydb, get_executions(myib))                   # inserta les noves (de 1 a 7 dies) executions
     dbupdate_executions(mydb, dbanalyse_executions(mydb,myaccId))   # actualitza les executions per tancar les que toqui
     dbfill_positions(mydb,dbanalyse_positions(mydb, myaccId))       # borra totes les positions i les re-inserta
 
